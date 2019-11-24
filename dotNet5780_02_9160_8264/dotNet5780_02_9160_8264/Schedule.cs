8f795d04485c3da8c508d3cb9e04335c688464b4
דוד אברג'ל <abrzel@acad.jct.ac.il>
2019-11-24 22:49:17 +0200

    added functions ב-ד

--- dotNet5780_02_9160_8264/dotNet5780_02_9160_8264/Schedule.cs
+++ dotNet5780_02_9160_8264/dotNet5780_02_9160_8264/Schedule.cs
@@ -94,5 +94,63 @@ namespace dotNet5780_02_9160_8264
             Console.WriteLine("Number of booked nights:{0}", numBookedNights);
             Console.WriteLine("Percentage of booked nights:{0}%", ((float)numBookedNights / (31 * 12)) * 100);
         }
+        public int GetAnnualBusyDays()
+        {
+            int sum = 0;
+            Console.WriteLine("Booked days in the year:");
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 31; j++)
+                {
+                    if (year[i, j])//we just found a new sequence
+                    {
+
+                        sum++;
+                    }
+
+                    
+                }
+            }
+            return sum;
+        }
+
+        public float GetAnnualBusyPercentage()
+        {
+            int numBookedNights = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 31; j++)
+                {
+                    if (year[i, j])
+                    {
+                        numBookedNights++;
+                    }
+                }
+            }
+            return (numBookedNights/372);
+
+        }
+        public bool ApproveRequest(GuestRequest guestReq)
+        {
+            int num_of_nights = ((int)guestReq.ReleaseDate.MonthIndex - (int)guestReq.EntryDate.MonthIndex) * 31 + (ending_day.DayIndex - guestReq.EntryDate.DayIndex);// calculates the length of visit in nights
+            bool allow = true;//is the booking still relevant
+            for (int i = 0; i < num_of_nights && allow; i++)
+            {
+                if (year[((int)guestReq.EntryDate.MonthIndex + (guestReq.EntryDate.DayIndex + i) / 31) % 12, (guestReq.EntryDate.DayIndex + i) % 31])
+                {
+                    Console.WriteLine("Your request was rejected: atleast one of the days you requested is occupied");
+                    allow = false;
+                }
+            }
+
+            if (allow)
+            {
+                for (int i = 0; i < num_of_nights; i++)
+                {
+                    year[((int)guestReq.EntryDate.MonthIndex + (guestReq.EntryDate.DayIndex + i) / 31) % 12, (guestReq.EntryDate.DayIndex + i) % 31] = true;//set day as booked
+                }
+                Console.WriteLine("Your request was accepted: days {0}.{1} - {2}.{3} are booked for you", guestReq.EntryDate.DayPrint, guestReq.EntryDate.MonthPrint, ending_day.DayPrint, ending_day.MonthPrint);
+            }
+        }
     }
 }
